22e8e0d44fb85ba1c33be0dfd4915b39c99dc728
Dhruv <dhruv.gupta.cse17@iitbhu.ac.in>
2025-07-26 17:13:35 +0530

    made expire more accurate

--- app/main.py
+++ app/main.py
@@ -18,12 +18,12 @@ def get_unix_time_ms():
     unix_ts_ms = int(unix_timestamp * 1000)
     return unix_ts_ms
 
-def create_response(msg):
+def create_response(msg, request_recv_time_ms):
     """
     create a response and return the redis protocol serialized version of it.
 
     default -> return PONG
-    supports ECHO command.
+    supports ECHO , GET <key>, SET <key> px <time_to_live_ms>.
     """
     result = b''
     if isinstance(msg, Iterable):
@@ -37,7 +37,10 @@ def create_response(msg):
             case b'GET':
                 key = tokens[1]
                 value_obj = redis_memstore.get(key, NULL_BULK_STRING)
-                if (value_obj.unix_expiry_ms != NO_EXPIRY) and (get_unix_time_ms() > value_obj.unix_expiry_ms):
+                if (value_obj.unix_expiry_ms != NO_EXPIRY) and (request_recv_time_ms > value_obj.unix_expiry_ms):
+                    print(f"{key=} expired")
+                    print(f"request time = {request_recv_time_ms}")
+                    print(f"expiry time = {value_obj.unix_expiry_ms}")
                     del redis_memstore[key]
                     return NULL_BULK_STRING
 
@@ -46,7 +49,7 @@ def create_response(msg):
             case b'SET':
                 key, val = tokens[1], tokens[2]
                 expire_ms = typecast_as_int(tokens[4]) if tokens[4] else NO_EXPIRY
-                expiry_time_ms = get_unix_time_ms() + expire_ms
+                expiry_time_ms = request_recv_time_ms + expire_ms
                 redis_memstore[key] = ValueObj(val=val, val_dtype=type(tokens[2]), unix_expiry_ms=expiry_time_ms)
                 return OK_SIMPLE_STRING
             case _:
@@ -64,11 +67,12 @@ async def handle_client(reader, writer):
 
     while True:
         data = await reader.read(MAX_MSG_LEN)
+        request_recv_time = get_unix_time_ms()
         if not data:
             print(f"Connection closed by {addr}")
             break
         print(f"Received from {addr}: {data}")
-        err_flag, message = parse_redis_bytes(data)
+        err_flag, message = parse_redis_bytes(data, request_recv_time)
         print(f"Parsed data: {message}")
 
         response = f"+PONG\r\n"
